7166d4cd54633bc0df8b60b5fe98d167f188bf69
Mark Azaev <mark@azaev.space>
2022-06-02 20:55:04 +0300

    Spring boot changes(deleted webconf)

--- src/main/java/com/mark/configurations/Application.java
+++ src/main/java/com/mark/configurations/Application.java
@@ -3,7 +3,7 @@ package com.mark.configurations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.mark")
 public class Application {
 
     public static void main(String[] args) {
